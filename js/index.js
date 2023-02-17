2e88e3a6d8ce3bd902f0964b8591fe12bb4d3a2a
naimurk <naimurk781@gmail.com>
2023-02-18 01:38:48 +0600

    rhombus card fucntion develop

--- js/index.js
+++ js/index.js
@@ -3,27 +3,67 @@ let serialNumber = 1
 
 // card 1
 document.getElementById('triangle-btn').addEventListener('click',function(){
-    const triangleName = document.getElementById('triangle').innerText
-    const triangleFieldb = document.getElementById('triangle-field-b').value
-    const triangleFieldh = document.getElementById('triangle-field-h').value
+    const shapName = document.getElementById('triangle').innerText
+    const shapFieldb = document.getElementById('triangle-field-b').value
+    const shapFielh = document.getElementById('triangle-field-h').value
 
-    const triangleFieldBNumber = parseFloat(triangleFieldb);
-    const triangleFieldHNumber = parseFloat(triangleFieldh);
+    const shapFieldbNumber = parseFloat(shapFieldb);
+    const shapFielhNumber = parseFloat(shapFielh);
     
-    if(triangleFieldb == '' || triangleFieldh == '' )
+    if(shapFieldb == '' || shapFielh == '' )
     {
        alert ('input daw')
     }
     else {
-        const sum = triangleFieldBNumber*triangleFieldHNumber*0.5;
+        const sum = shapFieldbNumber*shapFielhNumber*0.5;
         const answer = sum.toFixed(2)
-        getTableData(triangleName,answer)
+        getTableData(shapName,answer)
+        serialNumber += 1
+    }
+    
+})
+
+// card 2
+document.getElementById('rhombus-btn').addEventListener('click',function(){
+    const shapName = document.getElementById('rhombus').innerText
+    const shapFieldb = document.getElementById('rhombus-field-d1').value
+    const shapFielh = document.getElementById('rhombus-field-d2').value
+
+    const shapFieldbNumber = parseFloat(shapFieldb);
+    const shapFielhNumber = parseFloat(shapFielh);
+    
+    if(shapFieldb == '' || shapFielh == '' )
+    {
+       alert ('input daw')
+    }
+    else {
+        const sum = shapFieldbNumber*shapFielhNumber*0.5;
+        const answer = sum.toFixed(2)
+        getTableData(shapName,answer)
         serialNumber += 1
     }
     
 })
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// get table data
 function getTableData (Name , answer ) {
     const container = document.getElementById('table-container');
     const tr = document.createElement('tr')
